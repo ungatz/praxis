fbacae27752fe9a4fb5bc7a8ddac907796f31b45
cypherbeep <500068114@stu.upes.ac.in>
2022-03-16 17:06:12 +0530

    adding http-server in rust

--- languages/rust/http-server/src/main.rs
+++ languages/rust/http-server/src/main.rs
@@ -1,3 +1,37 @@
+use std::fs;
+use std::io::*;
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    // a listner to listen to incoming requests.
+    let listner = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listner.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let content = fs::read_to_string(filename).unwrap();
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        content.len(),
+        content
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
